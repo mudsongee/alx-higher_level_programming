f65983dc0df7071e1895eb143a0c0ad18cfb8ad8
akumande <mohdahmedjafar@gmail.com>
2023-05-11 19:58:36 -0700

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if(operator in "+-*/"):
-        if operator is "+":
+        if operator == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator is "-":
+        elif operator == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator is "*":
+        elif operator == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
         else:
             print("{} / {} = {}".format(a, b, div(a, b)))
