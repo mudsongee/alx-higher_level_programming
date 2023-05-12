8632b53215d1871ef430ddfa18cf532c9e233bdb
akumande <mohdahmedjafar@gmail.com>
2023-05-11 21:16:19 -0700

    Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
+def magic_calculation(a, b): 
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
@@ -8,7 +8,6 @@ def magic_calculation(a, b):
         return c
     else:
         return (sub(a, b))
-
 if __name__ == "__main__":
     import dis
     dis.dis(magic_calculation)
