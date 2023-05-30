59ac0d6879f5c18765db94493214d41940b06a5c
mudsongee <mohdahmedjafar@gmail.com>
2023-05-30 05:30:00 +0100

    Python - Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
+def magic_calculation(a, bi):
+    """Executes a function safely."""
+
     result = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
