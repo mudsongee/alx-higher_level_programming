73454d8123e7f48dfa2d9c58029a64d0893c5e90
mudsongee <mohdahmedjafar@gmail.com>
2023-06-07 04:13:09 +0100

    Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
-def magic_string(l=[]):
-    l.append('BestSchool')
-    return ', '.join(l)
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return 'BestSchool' + ', BestSchool' * (magic_string.count - 1)
