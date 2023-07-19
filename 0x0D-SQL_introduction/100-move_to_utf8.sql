9f85264aa34389b130bf0c98e099d6622fe77db8
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-07-19 03:44:58 +0100

    SQL - Introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,9 +2,9 @@
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
+/* ALTER TABLE hbtn.0c_0.first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; */
 
