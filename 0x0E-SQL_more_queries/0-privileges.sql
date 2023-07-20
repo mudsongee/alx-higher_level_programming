0441c3c3b7fb1c4b4783f0a60fbb423e924120be
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-07-20 03:27:13 +0100

    SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
--- lits all privilages
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- For user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- For user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
