591239ed463557977cfb2b3e224925073c5bfdc8
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-07-20 04:14:45 +0100

    SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,6 @@
 -- If user already exists, script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
 
