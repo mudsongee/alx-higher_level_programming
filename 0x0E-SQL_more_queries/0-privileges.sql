5554d721943b69b0553365218b9d0a26f067fb9e
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-07-20 03:29:56 +0100

    SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
--- For user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- For user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- SHOW GRANTS FOR 'username'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
