d5638b7bec580d20cd1bf4abde05b3cd7eec0990
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-08-15 13:52:08 +0100

    JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const args = process.argv[2];
-const parsedInteger = parsedInt(args);
 
-if (isNaN(parsedInteger)) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number: $parseInt(parsedInteger)');
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
