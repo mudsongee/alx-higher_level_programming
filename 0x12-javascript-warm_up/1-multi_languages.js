fa957d81c36908478b8438c2ac1d7036461d3089
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-08-15 04:33:24 +0100

    JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-
 const firstLine = 'C is fun';
-const secondLine = 'Python is cool';
-const thirdLine = 'JavaScript is amazing'
-console.log(firsLine);
-console.log(secondLine);
-console.log(thirdLine);
