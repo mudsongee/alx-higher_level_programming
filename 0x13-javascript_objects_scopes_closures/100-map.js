db716c689ebaf1f9ff65bd9e810d3d1d1cca65b8
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-08-15 17:45:19 +0100

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-let newList = list.map((x, i) => x * i);
+const newList = list.map((x, i) => x * i);
 console.log(list);
 console.log(newList);
