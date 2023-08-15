3aba97cb7bc8f173621e4f9992852bc87243e8a3
mudsongee <mohdahmedjafar@gmail.com>
2023-08-14 20:10:08 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < words.length; i++) {
   console.log(words[i]);
 }
-
