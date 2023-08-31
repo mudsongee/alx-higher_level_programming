9c59463d4759699892331221dac6958aaf8ef3f9
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-08-31 16:45:48 +0100

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #size of content-length
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+
+# Check if a URL is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url="$1"
+size=$(curl -sI "$url" | grep -i Content-Length | awk '{print $2}')
+echo "$size"
 
