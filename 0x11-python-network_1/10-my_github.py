1aee603a43b0f4037234f9dac940b3ea3d9d7c49
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-02 02:35:55 +0100

    Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,5 @@ from sys import argv
 try:
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     print(r.json().get('id'))
-except:
+except Exception as e:
     print("None")
-
