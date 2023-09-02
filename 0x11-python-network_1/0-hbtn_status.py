ec61b7945548c85ed009869ffe74f8e930f54029
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-02 01:25:49 +0100

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-fetch https://intranet.hbtn.io/status; display response
-"""
+# fetch https://intranet.hbtn.io/status; display response
 
 import urllib.request
 
@@ -14,4 +12,3 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(url))
-
