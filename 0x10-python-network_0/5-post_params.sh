cc57fdf051ad15aa884987fe7623814432ace8be
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-06 03:16:35 +0100

    Python - Network #0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
-curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"
 
