b766dc5e5dcd4b3aacff6ba4574f485bc2a90f39
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-06 03:05:31 +0100

    Python - Network #0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL and display only the body of a 200 status code response
-echo $(curl -s -o /dev/null -w '%{http_code}' "$1" | grep -q "200" && curl -sL "$1")
+curl -sL "$1"
