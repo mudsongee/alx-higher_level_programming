4fd897d764d2a67929e51cd4487c7929656dd55a
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-06 03:44:19 +0100

    Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+""" finds a peak in a list of unsorted integers. """
+
+
+def find_peak2(numbers, size, start, end):
+    middle = int(start + (end - start) / 2)
+
+    if ((middle is 0 or numbers[middle - 1] <= numbers[middle]) and
+       (middle == size - 1 or numbers[middle + 1] <= numbers[middle])):
+        return numbers[middle]
+    elif (middle > 0 and numbers[middle - 1] > numbers[middle]):
+        return find_peak2(numbers, size, start, (middle - 1))
+    else:
+        return find_peak2(numbers, size, middle + 1, end)
+
 
 def find_peak(list_of_integers):
-
-    if not list_of_integers:
+    if len(list_of_integers) == 0:
         return None
-    n = len(list_of_integers)
-    if n == 1:
+    if len(list_of_integers) == 1:
         return list_of_integers[0]
-    if n == 2:
-        return max(list_of_integers)
-    mid = n // 2
-    if list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    if list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid + 1:])
-    return list_of_integers[mid]
+    if len(list_of_integers) == 2:
+        return (list_of_integers[0] if list_of_integers[0] >
+                list_of_integers[1] else list_of_integers[1])
+    return (find_peak2(list_of_integers, len(list_of_integers), 0,
+            len(list_of_integers) - 1))
