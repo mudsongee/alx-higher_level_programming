885f142e40f0ef100fd50a5345679e6105e90751
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-28 16:45:55 +0100

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 // reads and prints the content of a file.
 
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
 const fs = require('fs');
 
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
