0b13db746b579ec71034d1bb9e8f77bb8777806d
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-28 18:48:58 +0100

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
-const request = require('request');
-const address = process.argv[2];
-
-request(address, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let results = JSON.parse(body).results;
-    let count = 0;
-    for (let i in results) {
-      for (let chr of results[i].characters) {
-        if (chr.search('/18/') > 0) { count += 1; }
-      }
-    }
-    console.log(count);
-  }
-});
-
+// prints the number of movies where the character “Wedge Antilles” is present
 const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18; // Character ID for Wedge Antilles
