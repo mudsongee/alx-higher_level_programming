b3fd4d51cf256d3594c92c0e955404981b6885cf
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-28 16:51:18 +0100

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,7 +8,5 @@ const stringToWrite = process.argv[3];
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log('File has been written successfully.');
   }
 });
