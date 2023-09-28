b3c0c2ffb879b347bb14e5ccaf98a8c108ef319d
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-28 19:26:55 +0100

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,23 @@
 // computes the number of tasks completed by user id.
 
 const request = require('request');
-const address = process.argv[2];
+const process = require('process');
+const apiUrl = process.argv[2];
 
-request(address, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    let results = {};
-    for (let td of JSON.parse(body)) {
-      if (td.completed) {
-        if (results[td['userId']] === undefined) { results[td['userId']] = 0; }
-        results[td['userId']] += 1;
+request.get(apiUrl, (err, response, body) => {
+  if (err === null) {
+    const data = JSON.parse(body);
+    const userIds = [...new Set(data.map(todo => todo.userId))];
+    const completedTasksData = {};
+    userIds.forEach((id) => {
+      const completedTasks = data.filter(todo => todo.userId === id && todo.completed);
+      if (completedTasks.length <= 0) {
+        return;
       }
-    }
-    console.log(results);
+      completedTasksData[id] = completedTasks.length;
+    });
+    console.log(completedTasksData);
+  } else {
+    console.log(err);
   }
 });
