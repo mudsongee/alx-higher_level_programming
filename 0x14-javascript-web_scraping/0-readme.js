13e392a5e32ec982cac7f914712241df54e69fe9
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-09-28 16:05:38 +0100

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,19 @@
-#!/usr/bin/node
-// prints the content of a file.
+const fs = require('fs');
 
-let file = process.argv[2];
-let fs = require('fs');
+// Check if the correct number of command-line arguments is provided
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <file_path>');
+  process.exit(1);
+}
 
-fs.readFile(file, 'utf8', (err, data) => {
+const filePath = process.argv[2];
+
+// Read the file in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
+
